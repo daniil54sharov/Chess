12a76d519d8ae592d5bfe7ebd4af5cf2f75897f0
daniil54sharov <daniilsharov54@gmail.com>
2023-10-25 20:06:50 +0300

    added gitignore

--- src/Test/TestClass.java
+++ src/Test/TestClass.java
@@ -6,33 +6,51 @@ import java.awt.*;
 import java.io.*;
 import java.awt.event.*;
 
-public class TestClass implements ActionListener, KeyListener{
+public class TestClass extends MouseAdapter implements ActionListener, KeyListener{
 	
 	Image dog = new ImageIcon("src/dog.jpg").getImage();
 	JLabel board[][];
+	JLabel label;
+	JFrame frame;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TestClass tc = new TestClass();
 		tc.go();
+		int i = 1;
+		int b = (i == 1) ? 2 : 1;
 	}
 	
 	public void go() {
-		JFrame frame = new JFrame("Image test");
-		JPanel mainPanel = new JPanel(new GridLayout(9, 9));
-		board = new JLabel[8][8];
-		
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8; j++) {
-				board[i][j] = new JLabel(new ImageIcon("src/dog.jpg"));
-				mainPanel.add(board[i][j]);
-			}
-		}
+		frame = new JFrame("Image test");
+		JPanel mainPanel = new JPanel();
+		label = new JLabel("asd");
+		label.addMouseListener(this);
+//		board = new JLabel[8][8];
+//		
+//		for(int i = 0; i < 8; i++) {
+//			for(int j = 0; j < 8; j++) {
+//				board[i][j] = new JLabel(new ImageIcon("src/dog.jpg"));
+//				mainPanel.add(board[i][j]);
+//			}
+//		}
+		mainPanel.add(label);
 		frame.setSize(550, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addKeyListener(this);
 		frame.add(mainPanel);
 		frame.setVisible(true);
+		
+		
+	}
+	
+	
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		super.mouseClicked(e);
+		System.out.println("gay sex");
 	}
 
 	@Override
--- src/mainPackage/Game.java
+++ src/mainPackage/Game.java
@@ -11,6 +11,7 @@ public class Game {
 	private JLabel board[][];
 	private JPanel mainPanel;
 	private ImageIcon[] black_figures;
+	private ImageIcon[] white_figures;
 	
 	public static void main(String[] args) {
 		Game chess = new Game();
@@ -21,48 +22,44 @@ public class Game {
 	    frame = new JFrame("Chess");
 	    board = new JLabel[8][8];
 	    mainPanel = new JPanel(new GridLayout(8, 8));
-	    black_figures = new ImageIcon[8];
+	    black_figures = new ImageIcon[9];
+	    white_figures = new ImageIcon[9];
 	    String icon_path = "src/img/icon.png";
 	    ImageIcon icon = new ImageIcon (icon_path);
 
-	    String[] black_figures_name = new String[] {"black_rock", "black_knight", "black_bishop", "black_king", "black_queen", "black_bishop", "black_knight", "black_rock"};
-	    for(int black_figure_counter = 0; black_figure_counter < 8; black_figure_counter++) {
+	    String[] black_figures_name = new String[] {"black_rock", "black_knight", 
+	    		"black_bishop", "black_queen", "black_king", "black_bishop", 
+	    		"black_knight", "black_rock", "black_pawn"};
+	    for(int black_figure_counter = 0; black_figure_counter < black_figures.length; black_figure_counter++) {
 	    	black_figures[black_figure_counter] = new ImageIcon("src/img/" + black_figures_name[black_figure_counter] + ".png");
 	    }
-	    String[] white_figures_name = new String[] {"white_rock", "white_knight", "white_bishop", "white_king", "white_queen", "white_bishop", "white_knight", "white_rock"};
+	    
+	    String[] white_figures_name = new String[] {"white_rock", "white_knight", 
+	    		"white_bishop", "white_queen", "white_king", "white_bishop", 
+	    		"white_knight", "white_rock", "white_pawn"};
+	    for(int white_figures_counter = 0; white_figures_counter < white_figures.length; white_figures_counter++) {
+	    	white_figures[white_figures_counter] = new ImageIcon("src/img/" + white_figures_name[white_figures_counter] + ".png");
+	    }
 	    
 	    for (int row = 0; row < 8; row++) {
 	        for (int col = 0; col < 8; col++) {
+	        	
 	            board[row][col] = new JLabel();
 	            board[row][col].setOpaque(true);
-	            if ((row + col) % 2 == 0) {
-	                board[row][col].setBackground(new Color(111, 78, 55));
-	            } else {
-	                board[row][col].setBackground(new Color(255,248,220));
-	            }
+	            board[row][col].setBackground((row + col) % 2 == 0 ? new Color(111, 78, 55) : new Color(255,248,220));
+	            board[row][col].addMouseListener(new BoardListener());
+	            
+	            if(row == 0) {
+	    			board[row][col].setIcon(black_figures[col]);
+	    		} else if(row == 1){
+	    			board[row][col].setIcon(black_figures[8]);
+	    		} else if(row == 7) {
+	    			board[row][col].setIcon(white_figures[col]);
+	    		} else if(row == 6) {
+	    			board[row][col].setIcon(white_figures[8]);
+	    		}
 	            mainPanel.add(board[row][col]);
 	        }
-	    }
-	    
-	    for(int black_row = 0; black_row < 2; black_row++) {
-	    	for(int black_col = 0; black_col < 8; black_col++) {
-	    		if(black_row == 0) {
-//	    			board[black_row][black_col].setText(black_figures_name[black_col]);
-	    			board[black_row][black_col].setIcon(black_figures[black_col]);
-	    		} else {
-	    			board[black_row][black_col].setText("black_pawn");
-	    		}
-	    	}
-	    }
-	    
-	    for(int white_row = 6; white_row < 8; white_row++) {
-	    	for(int white_col = 0; white_col < 8; white_col++) {
-	    		if(white_row == 7) {
-	    			board[white_row][white_col].setText(white_figures_name[white_col]);
-	    		} else {
-	    			board[white_row][white_col].setText("white_pawn");
-	    		}
-	    	}
 	    }
 	    
 	    frame.add(mainPanel);
@@ -71,5 +68,29 @@ public class Game {
 	    frame.setResizable(false);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setVisible(true);
+	}
+	
+	class BoardListener extends MouseAdapter {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			super.mouseClicked(e);
+			System.out.println("clicked");
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			super.mousePressed(e);
+			System.out.println("pressed");
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			super.mouseReleased(e);
+			System.out.println("released");
+		}
 	}
 }
